d9a421c77a746e81ad3da0ed1de7e25dcd9a1d3d
Ben Governali <bgovern93@gmail.com>
2019-04-16 15:50:02 -0400

    created madLibs.py

---
+++ firstProject.py
@@ -0,0 +1,7 @@
+FIRSTNAME = input('What is your first name? ')
+
+LASTNAME = input('What is your last name? ')
+
+COLOR = input('What is your favorite color? ')
+
+print("Hello %s %s! Your favorite color is %s." % (FIRSTNAME, LASTNAME, COLOR))
---
+++ madLibs.py
@@ -0,0 +1,7 @@
+ADJECTIVE  = input("Please enter an adjective: ")
+NOUN       = input("Please enter a noun: ")
+ADJECTIVE2 = input("Please enter another adjective: ")
+VERB       = input("Please enter a past tense verb: ")
+
+
+print("There once was a %s %s. He was so %s, everyone %s. The end!" % (ADJECTIVE, NOUN, ADJECTIVE2, VERB))
